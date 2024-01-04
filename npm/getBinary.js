bf776c534247badb732fb55de9a89b2eb266819c
ManiGhazaee <hosseinghazaee1@gmail.com>
2024-01-04 21:39:52 +0330

    binary-install v1.1.0

--- npm/getBinary.js
+++ npm/getBinary.js
@@ -19,8 +19,7 @@ function getBinary() {
     const platform = getPlatform();
     const url = `https://github.com/ManiGhazaee/ts-runtime-typechecker/releases/download/v${version}/ts-runtime-typechecker-${platform}.tar.gz`;
     console.log(url);
-    const name = "ts-runtime-typechecker";
-    return new Binary(url, { name });
+    return new Binary("ts-runtime-typechecker", url);
 }
 
 module.exports = getBinary;
